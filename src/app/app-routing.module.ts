e1d0a0802c3020a1595e12d9d8e3f3be592c4935
Carolina <carolina.reis@nava.com.br>
2022-05-06 03:33:32 -0300

    Criação do front-end da API Banline

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CorrentistaComponent } from './componentes/correntista/correntista.component';
+import { MovimentacaoListComponent } from './componentes/movimentacao-list/movimentacao-list.component';
+import { MovimentacaoNewComponent } from './componentes/movimentacao-new/movimentacao-new.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'movimentacoes', component :MovimentacaoListComponent},
+  {path: 'movimentacoes-new', component : MovimentacaoNewComponent},
+  { path: 'correntistas', component: CorrentistaComponent },
+  {path : '', redirectTo: 'movimentacoes', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
